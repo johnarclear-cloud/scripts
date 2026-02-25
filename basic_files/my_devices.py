82c7d827393764f2d9ded1771e531a5790e91161
john clear <johnarclear@gmail.com>
2026-02-25 21:07:09 +0000

    files

--- basic_files/my_devices.py
+++ basic_files/my_devices.py
@@ -1,4 +1,4 @@
 INVENTORY = [
-    {"hostname": "R1", "host": "192.168.68.201", "user": "john", "password": "Juniper1"},
-    {"hostname": "R2", "host": "192.168.68.202", "user": "john", "password": "Juniper1"}
+    {"hostname": "R1", "host": "192.168.68.220", "user": "john", "password": "Juniper1"},
+    {"hostname": "R2", "host": "192.168.68.221", "user": "john", "password": "Juniper1"}
 ]
---
+++ juniper-automation/test-prod/script1.py
@@ -0,0 +1,11 @@
+from nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_command
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="prod_config.yaml")
+
+def test_connect(task):
+    task.run(task=netmiko_send_command, command_string="show configuration")
+
+result = nr.run(task=test_connect)
+print_result(result)
