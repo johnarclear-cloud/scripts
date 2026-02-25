216ee296a1ad719727d58f3543f248af66ad38c8
john clear <johnarclear@gmail.com>
2026-02-25 21:23:04 +0000

    adding files

--- juniper-automation/test-prod/script1.py
+++ juniper-automation/test-prod/script1.py
@@ -1,8 +1,10 @@
 from nornir import InitNornir
 from nornir_netmiko.tasks import netmiko_send_command
 from nornir_utils.plugins.functions import print_result
+import sys 
 
-nr = InitNornir(config_file="prod_config.yaml")
+myconfig = sys.argv[1]
+nr = InitNornir(config_file=myconfig)
 
 def test_connect(task):
     task.run(task=netmiko_send_command, command_string="show configuration")
---
+++ juniper-automation/test-prod/script2.py
@@ -0,0 +1,5 @@
+import sys
+
+name = sys.argv[1]
+surname = sys.argv[2]
+print(F"my name is {name} {surname}")
